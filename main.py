bf8c4a24e3c95d4c20125bc511fe1fb21466d4c2
Manibod <virasonemanibod@hotmail.com>
2024-06-01 12:03:05 -0400

    chess board with real image piece

--- main.py
+++ main.py
@@ -13,6 +13,21 @@ TILE_SELECTED_BLACK_COLOR = (185, 202, 67)
 WHITE_PIECE_COLOR  = (249,249,249)
 BLACK_PIECE_COLOR  = (92,89,87)
 PIECE_BORDER_COLOR = (62,60,59)
+PIECE_FILENAME = "pieces.png"
+PIECE_IMAGE_NB_ROWS = 2
+PIECE_IMAGE_NB_COL = 6
+PIECE_IMAGE_WHITE_ROW = 0
+PIECE_IMAGE_BLACK_ROW = 1
+KING = 0
+QUEEN = 1
+BISHOP = 2
+KNIGHT = 3
+ROOK = 4
+PAWN = 5
+OFFSET1 = -3
+OFFSET2 = 1
+SCALING_W = 575
+SCALING_H = 200
 
 # Game constant
 LINE_MOVE = [[1, 0], [-1, 0], [0, 1], [0, -1]]
@@ -21,13 +36,6 @@ L_MOVE = [[ 1,  2], [ 2,  1],
           [-1,  2], [-2,  1],
           [-1, -2], [-2, -1],
           [ 1, -2], [ 2, -1]]
-
-ROOK   = "rook"
-KNIGHT = "knight"
-BISHOP = "bishop"
-QUEEN  = "queen"
-KING   = "king"
-PIECE_ORDER = [ROOK, KNIGHT, BISHOP, QUEEN, KING, BISHOP, KNIGHT, ROOK]
 
 WHITE = "white"
 PAWN_WHITE_MOVE = [[0,  1]]
@@ -52,26 +60,32 @@ class Pawn(Piece):
     def __init__(self, color):
         move_type = PAWN_WHITE_MOVE if color == WHITE else PAWN_BLACK_MOVE
         super().__init__(color, move_type)
-
+        self.id = PAWN
+        
 class Rook(Piece):
     def __init__(self, color):
         super().__init__(color, LINE_MOVE)
+        self.id = ROOK
 
 class Knight(Piece):
     def __init__(self, color):
         super().__init__(color, L_MOVE)
+        self.id = KNIGHT
 
 class Bishop(Piece):
     def __init__(self, color):
         super().__init__(color, DIAG_MOVE)
+        self.id = BISHOP
 
 class Queen(Piece):
     def __init__(self, color):
         super().__init__(color, LINE_MOVE + DIAG_MOVE)
+        self.id = QUEEN
 
 class King(Piece):
     def __init__(self, color):
         super().__init__(color, LINE_MOVE + DIAG_MOVE)
+        self.id = KING
 
 
 
@@ -80,10 +94,7 @@ class Game:
     def __init__(self):
         self.displayer = Displayer()
         self.run = True
-        self.board_2D = None
-        self.board_dict = None
         self.turn = WHITE
-
         self.init_boards()
 
     def init_boards(self):
@@ -138,6 +149,18 @@ class Displayer:
         pg.display.set_caption(TITLE)
         self.screen = pg.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
         self.selected_tile_pos = None
+        self.init_piece_image()
+
+    def init_piece_image(self):
+        self.pieces_img = pg.image.load(PIECE_FILENAME).convert_alpha()
+        self.pieces_img = pg.transform.scale(self.pieces_img, (SCALING_W, SCALING_H))
+        img_rect = self.pieces_img.get_rect()
+        w = img_rect.width // PIECE_IMAGE_NB_COL
+        h = img_rect.height // PIECE_IMAGE_NB_ROWS
+        self.piece_img = [
+            [(i % PIECE_IMAGE_NB_COL * w, 0, w, h) for i in range(PIECE_IMAGE_NB_COL)],
+            [(i % PIECE_IMAGE_NB_COL * w, h, w, h) for i in range(PIECE_IMAGE_NB_COL)]
+        ]
 
     def grid_display(self):
         self.screen.fill(TILE_BLACK_COLOR)
@@ -161,13 +184,16 @@ class Displayer:
 
     def board_display(self, board_dict):
         for pos, piece in board_dict.items():
-            color = WHITE_PIECE_COLOR if piece.color == WHITE else BLACK_PIECE_COLOR
-            x = (pos[0] * TILE_SIZE) + TILE_SIZE / 2
-            y = ((TILES_NB - 1 - pos[1]) * TILE_SIZE) + TILE_SIZE / 2
+            row = PIECE_IMAGE_WHITE_ROW if piece.color == WHITE else PIECE_IMAGE_BLACK_ROW
+            x = (pos[0] * TILE_SIZE)
+            if piece.id == ROOK or piece.id == ROOK or piece.id == PAWN:
+                x = x + OFFSET1
+            elif piece.id == KING:
+                x = x + OFFSET2
+            y = ((TILES_NB - 1 - pos[1]) * TILE_SIZE)
             screen_pos = (x, y)
-            pg.draw.circle(self.screen, color, screen_pos, TILE_SIZE / 2)
-            pg.draw.circle(self.screen, PIECE_BORDER_COLOR, screen_pos, TILE_SIZE / 2, 2)
-    
+            self.screen.blit(self.pieces_img, screen_pos, self.piece_img[row][piece.id])
+
     def set_selected_tile_pos(self, pos):
         self.selected_tile_pos = pos
 
