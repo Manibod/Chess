29dcfa02b7f8880e1a4ab04a2da1eb3c2cf242a5
Manibod <virasonemanibod@hotmail.com>
2024-06-01 21:13:48 -0400

    show possible moves knight

--- main.py
+++ main.py
@@ -10,9 +10,7 @@ TILE_WHITE_COLOR   = (235, 236, 208)
 TILE_BLACK_COLOR   = (115, 149, 82)
 TILE_SELECTED_WHITE_COLOR = (245, 246, 130)
 TILE_SELECTED_BLACK_COLOR = (185, 202, 67)
-WHITE_PIECE_COLOR  = (249,249,249)
-BLACK_PIECE_COLOR  = (92,89,87)
-PIECE_BORDER_COLOR = (62,60,59)
+POSSIBLE_MOVES_COLOR  = (92, 89, 87, 120)
 PIECE_FILENAME = "pieces.png"
 PIECE_IMAGE_NB_ROWS = 2
 PIECE_IMAGE_NB_COL = 6
@@ -30,20 +28,20 @@ SCALING_W = 575
 SCALING_H = 200
 
 # Game constant
-LINE_MOVE = [[1, 0], [-1, 0], [0, 1], [0, -1]]
-DIAG_MOVE = [[1, 1], [-1, 1], [-1, -1], [1, -1]]
-L_MOVE = [[ 1,  2], [ 2,  1],
-          [-1,  2], [-2,  1],
-          [-1, -2], [-2, -1],
-          [ 1, -2], [ 2, -1]]
+LINE_MOVE = [(1, 0), (-1, 0), (0, 1), (0, -1)]
+DIAG_MOVE = [(1, 1), (-1, 1), (-1, -1), (1, -1)]
+L_MOVE = [( 1,  2), ( 2,  1),
+          (-1,  2), (-2,  1),
+          (-1, -2), (-2, -1),
+          ( 1, -2), ( 2, -1)]
 
 WHITE = "white"
-PAWN_WHITE_MOVE = [[0,  1]]
+PAWN_WHITE_MOVE = (0,  1)
 PAWN_WHITE_ROW = 1
 PIECE_WHITE_ROW = 0
 
 BLACK = "black"
-PAWN_BLACK_MOVE = [[0, -1]]
+PAWN_BLACK_MOVE = (0, -1)
 PIECE_BLACK_ROW = 7
 PAWN_BLACK_ROW = 6
 
@@ -51,15 +49,15 @@ PAWN_BLACK_ROW = 6
 
 
 class Piece:
-    def __init__(self, color, move_type):
+    def __init__(self, color, move_delta):
         self.color = color
         self.has_moved = False
-        self.move_type = move_type
+        self.move_delta = move_delta
 
 class Pawn(Piece):
     def __init__(self, color):
-        move_type = PAWN_WHITE_MOVE if color == WHITE else PAWN_BLACK_MOVE
-        super().__init__(color, move_type)
+        move_delta = PAWN_WHITE_MOVE if color == WHITE else PAWN_BLACK_MOVE
+        super().__init__(color, move_delta)
         self.id = PAWN
         
 class Rook(Piece):
@@ -95,19 +93,21 @@ class Game:
         self.displayer = Displayer()
         self.run = True
         self.turn = WHITE
+        self.possible_moves = []
         self.init_boards()
 
     def init_boards(self):
         self.board_2D = [
-            [Rook(WHITE), Knight(WHITE), Bishop(WHITE), Queen(WHITE), King(WHITE), Bishop(WHITE), Knight(WHITE), Rook(WHITE)],
-            [Pawn(WHITE), Pawn(WHITE),   Pawn(WHITE),   Pawn(WHITE),  Pawn(WHITE), Pawn(WHITE),   Pawn(WHITE),   Pawn(WHITE)],
-            [None,        None,          None,          None,         None,        None,          None,          None       ],
-            [None,        None,          None,          None,         None,        None,          None,          None       ],
-            [None,        None,          None,          None,         None,        None,          None,          None       ],
-            [None,        None,          None,          None,         None,        None,          None,          None       ],
-            [Pawn(BLACK), Pawn(BLACK),   Pawn(BLACK),   Pawn(BLACK),  Pawn(BLACK), Pawn(BLACK),   Pawn(BLACK),   Pawn(BLACK)],
-            [Rook(BLACK), Knight(BLACK), Bishop(BLACK), Queen(BLACK), King(BLACK), Bishop(BLACK), Knight(BLACK), Rook(BLACK)]
+            [Rook(WHITE), Pawn(WHITE), None, None, None, None, Pawn(BLACK), Rook(BLACK)],
+            [Knight(WHITE), Pawn(WHITE), None, None, None, None, Pawn(BLACK), Knight(BLACK)],
+            [Bishop(WHITE), Pawn(WHITE), None, None, None, None, Pawn(BLACK), Bishop(BLACK)],
+            [Queen(WHITE), Pawn(WHITE), None, None, None, None, Pawn(BLACK), Queen(BLACK)],
+            [King(WHITE), Pawn(WHITE), None, None, None, None, Pawn(BLACK), King(BLACK)],
+            [Bishop(WHITE), Pawn(WHITE), None, None, None, None, Pawn(BLACK), Bishop(BLACK)],
+            [Knight(WHITE), Pawn(WHITE), None, None, None, None, Pawn(BLACK), Knight(BLACK)],
+            [Rook(WHITE), Pawn(WHITE), None, None, None, None, Pawn(BLACK), Rook(BLACK)]
         ]
+
         self.board_dict = {
             (0, 0): Rook(WHITE), (1, 0): Knight(WHITE), (2, 0): Bishop(WHITE), (3, 0): Queen(WHITE), (4, 0): King(WHITE), (5, 0): Bishop(WHITE), (6, 0): Knight(WHITE), (7, 0): Rook(WHITE),
             (0, 1): Pawn(WHITE), (1, 1): Pawn(WHITE), (2, 1): Pawn(WHITE), (3, 1): Pawn(WHITE), (4, 1): Pawn(WHITE), (5, 1): Pawn(WHITE), (6, 1): Pawn(WHITE), (7, 1): Pawn(WHITE),
@@ -121,6 +121,7 @@ class Game:
 
             self.displayer.grid_display()
             self.displayer.board_display(self.board_dict)
+            self.displayer.possible_moves_display(self.possible_moves)
             self.displayer.update()
         pg.quit()
 
@@ -131,14 +132,64 @@ class Game:
             elif event.type == pg.KEYDOWN:
                 if event.key == pg.K_ESCAPE:
                     self.run = False
-            elif event.type == pg.MOUSEBUTTONUP:
+            elif event.type == pg.MOUSEBUTTONDOWN:
                 mouse_pos = pg.mouse.get_pos()
                 screen_pos_x = mouse_pos[0] // TILE_SIZE
                 screen_pos_y = mouse_pos[1] // TILE_SIZE
                 pos = (screen_pos_x, TILES_NB - 1 - screen_pos_y)
+
                 if pos not in self.board_dict or self.board_dict[pos] == None:
                     pos = None
+
                 self.displayer.set_selected_tile_pos(pos)
+                self.possible_moves = self.get_possible_moves(pos)
+
+    def get_possible_moves(self, pos):
+        if pos == None or pos not in self.board_dict:
+            return []
+
+        piece = self.board_dict[pos]
+
+        if piece == None:
+            return []
+
+        if piece.color != self.turn:
+            return []
+
+        possible_moves = []
+
+        if piece.id == KING:
+            pass
+        elif piece.id == QUEEN:
+            pass
+        elif piece.id == BISHOP:
+            pass
+        elif piece.id == KNIGHT:
+            for delta in piece.move_delta:
+                possible_moves.append(add_pos(pos, delta))
+        elif piece.id == ROOK:
+            pass
+        elif piece.id == QUEEN:
+            pass
+        elif piece.id == PAWN:
+            pass
+
+        possible_moves = self.filter_moves(possible_moves, piece.color)
+
+        return possible_moves
+
+    def is_move_oob(self, pos):
+        return pos[0] < 0 or pos[0] >= TILES_NB or pos[1] < 0 or pos[1] >= TILES_NB
+
+    def is_move_same_color(self, pos, color):
+        x, y = pos
+        piece = self.board_2D[x][y]
+        return piece != None and piece.color == color
+    
+    def filter_moves(self, possible_moves, color):
+        return [move for move in possible_moves if
+                    not self.is_move_oob(move) and
+                    not self.is_move_same_color(move, color)]
 
 
 
@@ -193,12 +244,25 @@ class Displayer:
             y = ((TILES_NB - 1 - pos[1]) * TILE_SIZE)
             screen_pos = (x, y)
             self.screen.blit(self.pieces_img, screen_pos, self.piece_img[row][piece.id])
+    
+    def possible_moves_display(self, possible_moves):
+        for move in possible_moves:
+            x = (move[0] * TILE_SIZE) + TILE_SIZE / 2
+            y = ((TILES_NB - 1 - move[1]) * TILE_SIZE) + TILE_SIZE / 2
+            screen_pos = (x, y)
+            pg.draw.circle(self.screen, POSSIBLE_MOVES_COLOR, screen_pos, TILE_SIZE / 2, 5)
 
     def set_selected_tile_pos(self, pos):
         self.selected_tile_pos = pos
 
     def update(self):
         pg.display.update()
+
+
+
+
+def add_pos(pos1, pos2):
+    return (pos1[0] + pos2[0], pos1[1] + pos2[1])
 
 
 
